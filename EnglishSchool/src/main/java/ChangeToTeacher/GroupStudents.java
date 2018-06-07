e8568dad39bbd76d7f1a2a31738be8adf0b0a6c2
AleEscom03 <abuendiae1500@alumno.ipn.mx>
2018-06-07 16:56:29 -0500

    Resubida

--- EnglishSchool/src/main/java/ChangeToTeacher/GroupStudents.java
+++ EnglishSchool/src/main/java/ChangeToTeacher/GroupStudents.java
@@ -1,6 +1,8 @@
 package ChangeToTeacher;
 
 import entity.HibernateUtil;
+import entity.Usuarios;
+import entity.Grupo;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
@@ -8,10 +10,12 @@ import javax.faces.view.ViewScoped;
 import javax.servlet.http.HttpServletRequest;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import java.io.Serializable;
+import org.hibernate.Query;
 
 @ManagedBean
 @ViewScoped
-public class GroupStudents {
+public class GroupStudents implements Serializable {
 
     private final FacesContext fc;
     private final HttpServletRequest request;
@@ -20,16 +24,20 @@ public class GroupStudents {
     private List lstStudents;
 
     public GroupStudents() {
-        Session hibernateSession;
-        hibernateSession = HibernateUtil.getSessionFactory().openSession();
-        Transaction t = hibernateSession.beginTransaction();
-        String hql = "from Usuarios ";
         fc = FacesContext.getCurrentInstance();
         request = (HttpServletRequest) fc.getExternalContext().getRequest();
         if (request.getSession().getAttribute("sesionusuario") != null) {
             id = (String) request.getSession().getAttribute("sesionusuario");
             nombre = (String) request.getSession().getAttribute("nombre");
         }
+        Session hibernateSession;
+        hibernateSession = HibernateUtil.getSessionFactory().openSession();
+        Transaction t = hibernateSession.beginTransaction();
+        Usuarios p = (Usuarios) hibernateSession.createQuery("from Usuarios where idLog = '" + id + "' ").uniqueResult();
+        Grupo g = (Grupo) hibernateSession.createQuery("from Grupo where idUsuarios = " + p.getIdUsuarios() + "").uniqueResult();
+        String hql = "select u.idUsuarios,u.nombre,u.apellido,u.correo,u.contraseña from alumno_grupo a, usuarios u where a.idGrupo = " + g.getIdGrupo() + " and a.idUsuarios=u.idUsuarios;";
+        Query consulta = hibernateSession.createSQLQuery(hql);
+        lstStudents = consulta.list();
     }
 
     public String getId() {

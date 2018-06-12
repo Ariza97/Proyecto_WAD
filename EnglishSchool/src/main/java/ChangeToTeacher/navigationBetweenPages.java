75b8907fe034554c3c91a113cfaf8a9c56079aba
Ariza97 <arizamtz2303@gmail.com>
2018-06-12 01:42:14 -0500

    Agregando canva para elaboracion de ejercicios

--- EnglishSchool/src/main/java/ChangeToTeacher/navigationBetweenPages.java
+++ EnglishSchool/src/main/java/ChangeToTeacher/navigationBetweenPages.java
@@ -2,12 +2,18 @@ package ChangeToTeacher;
 
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 
 @ManagedBean
 @ViewScoped
+@RequestScoped
 public class navigationBetweenPages implements Serializable {
 
+    private String jdom;
+    
     public navigationBetweenPages() {
     }
 
@@ -46,9 +52,47 @@ public class navigationBetweenPages implements Serializable {
         System.out.println("Cambiando a pagina WelcomeTeacher");
         return "Exercises";
     }
-    public String toHW_1() {
-        System.out.println("Cambiando a pagina WelcomeTeacher");
-        return "HW_1";
-    }    
+    public String toHW_1(String num_ejercicio) {
+        System.out.println("Cambiando a pagina HW");
+     FacesContext fc;
+     HttpServletRequest request;
+     String id;
+     String nombre;
+     String grupo;
     
+        fc = FacesContext.getCurrentInstance();
+        request = (HttpServletRequest) fc.getExternalContext().getRequest();
+        if (request.getSession().getAttribute("sesionusuario") != null) {
+            id = (String) request.getSession().getAttribute("sesionusuario");
+            nombre = (String) request.getSession().getAttribute("nombre");
+            grupo = (String) request.getSession().getAttribute("grupo");
+        System.out.println("Grupo:" +grupo);
+        System.out.println("num ejercicio:" +num_ejercicio);
+            cargarJDOM(grupo, num_ejercicio);
+        }
+        return "HW";
+    }
+    public void cargarJDOM(String Grupo, String Nivel){
+        if(Grupo.equals("1CM1")){
+            switch (Nivel) {
+                case "uno":
+                    jdom ="{\"version\":\"2.2.4\",\"objects\":[]}";
+                    System.out.println(jdom);
+                    break;
+                case "dos":
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public String getJdom() {
+        return jdom;
+    }
+
+    public void setJdom(String jdom) {
+        this.jdom = jdom;
+    }
+
 }
